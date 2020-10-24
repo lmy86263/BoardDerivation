8e7cda901b01986c4f2db53653b315ea2aeaab67
lmy86263 <lmy_86263@163.com>
2020-10-24 17:33:00 +0800

    perceptron test

--- linear_classification/perceptron/perceptron.py
+++ linear_classification/perceptron/perceptron.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, max_iteration=100, learning_rate=0.001):
+    def __init__(self, max_iteration=1e5, learning_rate=0.01):
         self.weight = None
         self.max_iteration = max_iteration
         self.learning_rate = learning_rate
 
     def fit(self, X, y):
-        self.weight = np.random.randn(X.shape[0], 1)
+        self.weight = np.random.randn(X.shape[1], 1)
         # train with all examples
-        for _ in range(self.max_iteration):
+        for _ in range(int(self.max_iteration)):
             y_pred = self.predict(X)
             mask = y_pred <= 0
             error_count = np.count_nonzero(mask)
@@ -18,10 +18,13 @@ class Perceptron:
                 break
             else:
                 # update
-                self.weight += self.learning_rate * (X[mask] @ y[mask].T)
+                a = X[mask].T
+                b = y.reshape(y.shape[0], 1)[mask]
+                gradient = - a @ b
+                self.weight -= self.learning_rate * gradient
 
     def predict(self, X):
-        return np.sign(self.weight.T @ X)
+        return np.sign(X @ self.weight).ravel()
 
 
 if __name__ == '__main__':
