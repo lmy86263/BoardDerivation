7faf542de06db119e9134e4571eb0ca92b458c8f
lmy86263 <lmy_86263@163.com>
2020-11-08 23:56:09 +0800

    update visual test for markov chain

--- mcmc/MCMC/markov_chain_test.py
+++ mcmc/MCMC/markov_chain_test.py
@@ -1,18 +1,23 @@
 from MCMC.markov_chain import MarkovChain
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 if __name__ == '__main__':
     states = ['sleep', 'coding', 'derivation']
     transition_matrix = [[0.1, 0.5, 0.4], [0.6, 0.05, 0.35], [0.2, 0.7, 0.1]]
     mc = MarkovChain(transition_matrix, states=states)
 
-    all_sequences = []
-    steps = [1000, 5000, 10000, 20000, 50000, 70000, 100000]
-    for i in steps:
-        sequence, _ = mc.generate_sequence(init_state='sleep', steps=1000)
-        all_sequences.append(sequence)
+    n_steps = 20000
+    sequence, _ = mc.generate_sequence(init_state='sleep', steps=n_steps)
+    sequence_index = np.array([states.index(value) for value in sequence])
 
-    labels = [str(step) for step in steps]
-    plt.hist(all_sequences, histtype='bar', label=labels)
-    plt.legend()
+    fig, ax = plt.subplots()
+
+    offsets = range(1, n_steps, 5)
+    for i, label in enumerate(states):
+        ax.plot(offsets, [np.sum(sequence_index[:offset] == i) / offset for offset in offsets], label=label)
+    ax.set_xlabel("number of steps")
+    ax.set_ylabel("target distribution")
+    ax.legend(frameon=False)
     plt.show()
